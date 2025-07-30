7e7320afaaff3198df827321d4b581c5141f142b
sirajhanifa <sirajhanifa786@gmail.com>
2025-07-30 21:14:02 +0530

    changes

---
+++ controller/userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/user')
+
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username, password });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // Login success
+        res.status(200).json({ message: 'Login successful', user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+module.exports = { loginUser };
--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require('express');
-const app = express();\
-require('dotenv').config();
+const connectDB = require('./config/db');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
 
-// Middleware to parse JSON
+
+// Load environment variables
+require('dotenv').config({ quiet: true });
+
+// Initialize Express app
+const app = express();
+
+
+//Import Models
+const User = require('./models/user')
+
+
+
+//Routes
+const login = require('./routes/userRoutes')
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
 app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('Hello from Express backend!');
-});
+// Enable CORS for all origins
+app.use(cors());
+
+
+// Set the routing
+app.use('/api', login)
+
+
 
 // Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
